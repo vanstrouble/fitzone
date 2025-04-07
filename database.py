856da26843d2affc23343c496ac464715f1b2b4c
Pedro Vázquez <vanstrouble@proton.me>
2025-04-07 16:38:52 -0600

    Refactor database setup in database.py to streamline session creation and remove unused imports

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./fitzone.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Create a new database
-DATABASE_URL = "sqlite:///fitzone.db"
-engine = create_engine(DATABASE_URL, echo=False)
+Base.metadata.create_all(bind=engine)
 
-# Create a session
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
